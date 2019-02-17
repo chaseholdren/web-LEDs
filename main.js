ba5ea40d861948b1d48f021850f40de4960f98f2
Chase Holdren <chase.holdren@onbase.com>
2019-02-17 15:36:28 -0500

    stuff

--- main.js
+++ main.js
@@ -1,8 +1,75 @@
-var serialport = require('serialport');
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const firebase = require("firebase");
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDIl_SsxnPVi7Ys5I2W9j4pCZiz0fxBOvg",
+    authDomain: "dweenoh.firebaseapp.com",
+    databaseURL: "https://dweenoh.firebaseio.com",
+    projectId: "dweenoh",
+    storageBucket: "dweenoh.appspot.com",
+    messagingSenderId: "1097424934628"
+};
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+db.collection("ledData").doc("PJXXO3q2N1pr10D3Yn5c")
+    .onSnapshot(function (doc) {
+        const {
+            leds
+        } = doc.data();
+        console.log("leds: ", leds);
+    });
+
+
+const onOpen = (port) => {
+    port.write("50");
+}
 
 // list serial ports:
-serialport.list(function (err, ports) {
+SerialPort.list(function (err, ports) {
     ports.forEach(function (port) {
         console.log(port.comName);
     });
 });
+
+const com3port = new SerialPort('COM3', {
+    baudRate: 115200,
+    autoOpen: false,
+});
+
+com3port.open(function (err) {
+    if (err) {
+        return console.log('Error opening port: ', err.message)
+    }
+    // setTimeout(() => {
+    //     onOpen(com3port);
+    // }, 3000);
+});
+
+// Open errors will be emitted as an error event
+com3port.on('error', function (err) {
+    console.log('Error: ', err.message)
+})
+
+com3port.on('data', function (data) {
+    console.log('Data:', data);
+});
+
+const parser = com3port.pipe(new Readline());
+
+parser.on('data', (data) => {
+    console.log(data);
+    console.log("hi");
+    if (data.trim() === "setup4") {
+        console.log("hi2");
+        // com3port.write("200");
+    }
+})
+
+// The open event is always emitted
+com3port.on('open', function () {
+    // open logic
+    onOpen(com3port);
+})
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 #define LED_PIN 8
 #define NUM_LEDS 256
-#define BRIGHTNESS 2
+#define BRIGHTNESS 50
 #define LED_TYPE WS2811
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
@@ -15,23 +15,44 @@ TBlendType currentBlending;
 
 void setup()
 {
+  Serial.println("setup1");
   delay(3000); // power-up safety delay
+  Serial.println("setup2");
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
 
   currentPalette = LavaColors_p;
   currentBlending = LINEARBLEND;
-}
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for native USB
+  }
 
-static uint8_t colorIndex = 0;
+  // static uint8_t colorIndex = 255;
+  // leds[colorIndex % NUM_LEDS] = CRGB::Red;
+
+  // FastLED.show();
+  Serial.println("setup3");
+  Serial.begin(115200);
+  Serial.println("setup4");
+}
 
 void loop()
 {
-  colorIndex = colorIndex + 1; /* motion speed */
+  if (Serial.available() > 0)
+  {
+    int inputString = Serial.parseInt();
+    Serial.flush();
+    Serial.println(inputString);
+    // colorIndex = colorIndex + 1; /* motion speed */
+    if (inputString > 0 && inputString < NUM_LEDS)
+    {
 
-  FastLED.clear();
-  leds[colorIndex % NUM_LEDS] = CRGB::Red;
+      FastLED.clear();
+      leds[inputString % NUM_LEDS] = CRGB::Red;
 
-  FastLED.show();
-  FastLED.delay(1000 / UPDATES_PER_SECOND);
+      FastLED.show();
+    }
+  }
+  // delay(1000);
 }
